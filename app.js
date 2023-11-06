9b9f278ebefd69fac42e6042151fb80141bb9b17
Taj <tajmarie92@gmail.com>
2023-11-06 10:44:53 -0600

    Completed the exercise

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ console.log(
 );
 
 // Exercise 1
+const firtName = "Taj"
+let lastName = "Roller"
+var age = 31
 
 // Exercise 2
+let language = "JavaScript"
+let createdYear = 1985
+let isCaseSensitive = true
+
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
 
 // Exercise 3
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
